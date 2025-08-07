8df095872368b3a0b21317aed602344325c6e286
Sahil sanjay more <108298194+sahilprojgith@users.noreply.github.com>
2025-08-08 03:48:34 +0530

    Delete spring-boot-rest directory

--- spring-boot-rest/src/main/java/com/telusko/spring_boot_rest/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.telusko.spring_boot_rest.repo;
-
-
-import com.telusko.spring_boot_rest.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
